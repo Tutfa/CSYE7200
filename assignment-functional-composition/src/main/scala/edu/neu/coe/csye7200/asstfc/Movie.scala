f6afd3fbc919ce6fdb77fdb10f7652ddd9f5615f
DJia <jia.21big@gmail.com>
2021-03-03 16:20:57 +0800

    Assignment5

--- assignment-functional-composition/src/main/scala/edu/neu/coe/csye7200/asstfc/Movie.scala
+++ assignment-functional-composition/src/main/scala/edu/neu/coe/csye7200/asstfc/Movie.scala
@@ -1,9 +1,10 @@
 package edu.neu.coe.csye7200.asstfc
 
+import spray.json._
+
 import scala.collection.mutable
 import scala.io.{Codec, Source}
 import scala.util._
-import spray.json._
 
 /**
   * This is a variation on the previous Movie class (in edu.neu.coe.csye._7200.ingest)
@@ -102,7 +103,16 @@ object Movie extends App {
   object MoviesProtocol extends DefaultJsonProtocol {
     // 20 points
     // TO BE IMPLEMENTED
-    ???
+
+    implicit val formatFormat = jsonFormat4(Format.apply)
+    implicit val productionFormat = jsonFormat4(Production.apply)
+    implicit val nameFormat = jsonFormat4(Name.apply)
+    implicit val ratingFormat = jsonFormat2(Rating.apply)
+    implicit val reviewsFormat = jsonFormat7(Reviews.apply)
+    implicit val principalFormat = jsonFormat2(Principal.apply)
+    implicit val movieFormat = jsonFormat11(Movie.apply)
+    case class Movies(movies: Seq[Movie])
+    implicit val seqMovieFormat = jsonFormat1(Movies)
   }
 
   implicit object IngestibleMovie extends IngestibleMovie
@@ -119,11 +129,15 @@ object Movie extends App {
     source.close()
   }
 
+  import MoviesProtocol._
+
   //Hint: Serialize the input to Json format and deserialize back to Object, check the result is still equal to original input.
   def testSerializationAndDeserialization(ms: Seq[Movie]): Boolean = {
     // 5 points
     // TO BE IMPLEMENTED
-    ???
+    val jFormat = ms.toJson.toString
+    def fromJson (string: String) = string.parseJson.convertTo[Seq[Movie]]
+    fromJson(jFormat).equals(ms)
   }
 
   def getMoviesFromCountry(country: String, movies: Iterator[Try[Movie]]): Try[Seq[Movie]] = {
